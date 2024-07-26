5ea21b2b5142d29ab492021cf6d495bad01b421a
niccostantini <costantiniccolo@gmail.com>
2024-07-27 01:50:55 +0200

    Fix reset logic and error handling if n < 100

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
 const container = document.querySelector(".container");
 const squareSide = 31.04;
+const resetButton = document.querySelector(".reset");
+const number = document.querySelector(".number");
+const createButton = document.querySelector(".create");
 let squares;
 
+resetButton.disabled = true;
+
 function createGrid (n) {
-    for(i = 0; i < (n*n); i++){
-        let square = document.createElement("div");
-        square.setAttribute("class", "square");
-        container.appendChild(square);
+
+    if (n > 100) {container.textContent = "ERROR"}
+    else if (n <= 100) {
+        if (container.textContent == "ERROR") {container.textContent = ""}
+        for(i = 0; i < (n*n); i++){
+            let square = document.createElement("div");
+            square.setAttribute("class", "square");
+            container.appendChild(square);
+        }
+        let containerWidth = (squareSide * n * 1.02)+"px";
+        container.style.width = containerWidth;
+        squares = document.querySelectorAll(".square");
+        squares.forEach(square => square.addEventListener("mouseover", () => {square.style.backgroundColor = "black"}))
+        createButton.disabled = true;
+        resetButton.disabled = false;
+        }
     }
-    let containerWidth = (squareSide * n * 1.02)+"px";
-    container.style.width = containerWidth;
-    squares = document.querySelectorAll(".square");
-    squares.forEach(square => square.addEventListener("mouseover", () => {square.style.backgroundColor = "black"}))
-}
 
 function reset() {
-
+    squares = document.querySelectorAll(".square");
+    squares.forEach(function(square) {square.style.backgroundColor = ""; square.remove();});
+    resetButton.disabled = true;
 }
+
+createButton.addEventListener("click", function() {createGrid(number.value);})
+
+resetButton.addEventListener("click", function() {reset(); createButton.disabled = false;});
+
