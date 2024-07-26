ad7153caba825ed800dd412ad49d538d0342f382
niccostantini <costantiniccolo@gmail.com>
2024-07-26 18:27:10 +0200

    Create grid-generation function

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 const container = document.querySelector(".container");
+const squareSide = 31.04;
 
 function createGrid (n) {
-
-    for(i = o; i < n; i++){
-
+    for(i = 0; i < (n*n); i++){
+        let square = document.createElement("div");
+        square.setAttribute("class", "square");
+        container.appendChild(square);
     }
 
+
+    let containerWidth = (squareSide * n * 1.02)+"px";
+
+    container.style.width = containerWidth;
 }
