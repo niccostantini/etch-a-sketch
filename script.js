531bc12249b56e15be8211fc868e182fd6bc0f4e
niccostantini <costantiniccolo@gmail.com>
2024-07-28 20:42:34 +0200

    Start writing function for disabling choosen color button and enabling choosable ones.

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 const container = document.querySelector(".container");
-const squareSide = 31.04;
+const containerWidth = container.offsetHeight;
 const resetButton = document.querySelector(".reset");
 const number = document.querySelector(".number");
 const createButton = document.querySelector(".create");
+const blackButton = document.querySelector("#black");
+let colorButtons = document.querySelectorAll(".color")
+let squareColor = "black";
 let squares;
+let squareDim;
+
+blackButton.disabled = true;
+blackButton.classList.add("active");
+let activeButton;
+blackButton.classList
 
 resetButton.disabled = true;
 
 function createGrid (n) {
 
-    if (n > 100) {container.textContent = "ERROR"; resetButton.disabled = true;}
+    if (n > 100) {container.textContent = "!!!ERROR!!!"; resetButton.disabled = false; createButton.disabled = true}
     else if (n <= 100) {
         if (container.textContent == "ERROR") {container.textContent = ""}
         for(i = 0; i < (n*n); i++){
             let square = document.createElement("div");
+            squareDim = (parseInt(containerWidth)/n) + "px";
             square.setAttribute("class", "square");
+            square.style.width = squareDim; square.style.height = squareDim;
             container.appendChild(square);
         }
-        let containerWidth = (squareSide * n + 1)+"px";
-        container.style.width = containerWidth;
+        squareDim = (container.style.offsetWidth / n) + "px";
         squares = document.querySelectorAll(".square");
-        squares.forEach(square => square.addEventListener("mouseover", () => {square.style.backgroundColor = "black"; square.style.borderColor = "black"}))
+        squares.forEach(square => square.addEventListener("mouseover", () => {square.style.backgroundColor = squareColor; square.style.borderColor = squareColor;}))
         createButton.disabled = true;
         resetButton.disabled = false;
         }
@@ -29,9 +39,22 @@ function createGrid (n) {
 function reset() {
     squares = document.querySelectorAll(".square");
     squares.forEach(function(square) {square.style.backgroundColor = ""; square.style.borderColor = ""; square.remove();});
-    container.style.width = 0;
     resetButton.disabled = true;
 }
+
+function chooseBrush(button) {
+
+activeButton = document.querySelector(".active")
+    activeButton.disabled = false;
+activeButton.classList.remove(".active");
+    activeButton.classList.add(".inactive");
+    button.classList.remove(".inactive");
+    button.classList.add(".active");
+    button.disabled = true;
+
+}
+
+colorButtons.forEach(b => b.addEventListener("click", chooseBrush(b)));
 
 createButton.addEventListener("click", function() {createGrid(number.value);})
 
